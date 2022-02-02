602260d30e9f66f47751940f45a5b5a74599bf45
ung1ung2 <98893435+ung1ung2@users.noreply.github.com>
2022-02-02 21:14:42 +0700

    Add files via upload

---
+++ lib/util/places.dart
@@ -0,0 +1,62 @@
+List places = [
+  {
+    "name": "Hukum Bacaan Tajwid dalam Al-Qur'an",
+    "img": "assets/2.jpeg",
+    "Category": r"Nun mati dan tanwin",
+    "Type": "Al-Quran",
+    "details": "Nun sukun dan tanwin adalah hukum bacaan nun mati"
+        "bertemu dengan salah satu huruf yang sudah dibagi dalam"
+        "congue leo eget malesuada. "
+        "\nbeberapa kategori. Hukum nun sukun dan tanwin ada lima"
+        "macam yaitu, izar halqi, idgham bighunnah, idghom"
+        " bilaguhunnah, iqlab, dan ikfa haqiqi.",
+  },
+  {
+    "name": "Hukum Bacaan Tajwid dalam Al-Qur'an",
+    "img": "assets/1.jpeg",
+    "Category": r"Nun mati dan tanwin",
+    "Type": "Al-Quran",
+    "details": "Nun sukun dan tanwin adalah hukum bacaan nun mati"
+        "bertemu dengan salah satu huruf yang sudah dibagi dalam"
+        "congue leo eget malesuada. "
+        "\nbeberapa kategori. Hukum nun sukun dan tanwin ada lima"
+        "macam yaitu, izar halqi, idgham bighunnah, idghom"
+        " bilaguhunnah, iqlab, dan ikfa haqiqi.",
+  },
+  {
+    "name": "Hukum Bacaan Tajwid dalam Al-Qur'an",
+    "img": "assets/3.jpeg",
+    "Category": r"Nun mati dan tanwin",
+    "Type": "Al-Quran",
+    "details": "Nun sukun dan tanwin adalah hukum bacaan nun mati"
+        "bertemu dengan salah satu huruf yang sudah dibagi dalam"
+        "congue leo eget malesuada. "
+        "\nbeberapa kategori. Hukum nun sukun dan tanwin ada lima"
+        "macam yaitu, izar halqi, idgham bighunnah, idghom"
+        " bilaguhunnah, iqlab, dan ikfa haqiqi.",
+  },
+  {
+    "name": "Hukum Bacaan Tajwid dalam Al-Qur'an",
+    "img": "assets/2.jpeg",
+    "Category": r"Nun mati dan tanwin",
+    "Type": "Al-Quran",
+    "details": "Nun sukun dan tanwin adalah hukum bacaan nun mati"
+        "bertemu dengan salah satu huruf yang sudah dibagi dalam"
+        "congue leo eget malesuada. "
+        "\nbeberapa kategori. Hukum nun sukun dan tanwin ada lima"
+        "macam yaitu, izar halqi, idgham bighunnah, idghom"
+        " bilaguhunnah, iqlab, dan ikfa haqiqi.",
+  },
+  {
+    "name": "Hukum Bacaan Tajwid dalam Al-Qur'an",
+    "img": "assets/1.jpeg",
+    "Category": r"Nun mati dan tanwin",
+    "Type": "Al-Quran",
+    "details": "Nun sukun dan tanwin adalah hukum bacaan nun mati"
+        "bertemu dengan salah satu huruf yang sudah dibagi dalam"
+        "congue leo eget malesuada. "
+        "\nbeberapa kategori. Hukum nun sukun dan tanwin ada lima"
+        "macam yaitu, izar halqi, idgham bighunnah, idghom"
+        " bilaguhunnah, iqlab, dan ikfa haqiqi.",
+  },
+];
